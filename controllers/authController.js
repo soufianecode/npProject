cc33bebed55e015826b79f972cd528b184aa7d55
Soufiane El kabir <elkabir.soufiane.pro@gmail.com>
2020-11-04 21:51:01 +0100

    User model + mangoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,10 @@
+const User = require('../models/User');
+
+//handle errors
+const handleErrors =(err) =>{
+    console.log(err.message, err.code);
+}
+
 module.exports.signup_get = (req , res) =>{
     res.render('signup');
 }
@@ -6,11 +13,18 @@ module.exports.login_get = (req , res) =>{
     res.render('login');
 }
 
-module.exports.signup_post = (req , res) =>{
+module.exports.signup_post = async (req , res) =>{
     const { email, password } = req.body
 
-    console.log( email, password);
-    res.send('new signup');
+    try{
+        const user = await User.create({email, password});
+        res.status(201).json(user);
+    }
+    catch (err) {
+        handleErrors(err);
+        res.status(400).send('error, user not created');
+    }
+
 }
 
 module.exports.login_post = (req , res) =>{
---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { isEmail } = require("validator");
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "please enter an email"],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, "please enter a valid email"]
+    },
+    password: {
+        type: String,
+        required: [true, "please enter a password"],
+        minlength: [6, "Minimum password length is 6 characters"]
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
